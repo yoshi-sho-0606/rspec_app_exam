abf599a048cd375b28a3cf2d01df4e4e08ed9b5c
sho <yoshi0606@outlook.jp>
2021-01-30 16:47:35 +0900

    Fix: 変数名の変更をしました

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -78,13 +78,13 @@ RSpec.describe 'Task', type: :system do
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
-        task = create(:task, :done)
-        visit edit_project_task_path(project, task)
+        done_task = create(:task, :done)
+        visit edit_project_task_path(project, done_task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
         expect(page).to have_content('todo')
         expect(page).not_to have_content(Time.current.strftime('%Y-%m-%d'))
-        expect(current_path).to eq project_task_path(project, task)
+        expect(current_path).to eq project_task_path(project, done_task)
       end
     end
   end
